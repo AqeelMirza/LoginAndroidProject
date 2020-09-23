96dc66bec114ac8f9affc93091e7582e23138e4e
Aqeel <aqeel.mirza93@gmail.com>
2020-09-23 10:59:00 +1000

    Kodein DI implemented in LoginActivity.kt

---
+++ app/src/main/java/com/mirza/nowmoneytest/NowApplication.kt
@@ -0,0 +1,29 @@
+package com.mirza.nowmoneytest
+
+import android.app.Application
+import com.mirza.nowmoneytest.network.MyApi
+import com.mirza.nowmoneytest.network.NetworkConnectionInterceptor
+import com.mirza.nowmoneytest.repo.UserRepo
+import com.mirza.nowmoneytest.ui.login.LoginViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+class NowApplication : Application(), KodeinAware {
+
+    override val kodein = Kodein.lazy {
+        import(androidXModule(this@NowApplication))
+
+        bind() from singleton { NetworkConnectionInterceptor(instance()) }
+        bind() from singleton { MyApi(instance()) }
+       // bind() from singleton { AppDatabase(instance()) }
+        bind() from singleton { UserRepo(instance()) }
+        bind() from provider { LoginViewModelFactory(instance()) }
+
+
+    }
+}
--- app/src/main/java/com/mirza/nowmoneytest/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mirza/nowmoneytest/ui/login/LoginActivity.kt
@@ -1,28 +1,27 @@
 package com.mirza.nowmoneytest.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.mirza.nowmoneytest.R
 import com.mirza.nowmoneytest.databinding.ActivityLoginBinding
-import com.mirza.nowmoneytest.network.MyApi
-import com.mirza.nowmoneytest.network.NetworkConnectionInterceptor
-import com.mirza.nowmoneytest.repo.UserRepo
 import com.mirza.nowmoneytest.util.hide
-import com.mirza.nowmoneytest.util.toast
 import com.mirza.nowmoneytest.util.show
 import com.mirza.nowmoneytest.util.snackbar
+import com.mirza.nowmoneytest.util.toast
 import kotlinx.android.synthetic.main.activity_login.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class LoginActivity : AppCompatActivity(), LoginListener {
+class LoginActivity: AppCompatActivity(), LoginListener,KodeinAware {
+
+    override val kodein by kodein()
+    private val factory: LoginViewModelFactory by instance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val networkConnectionInterceptor = NetworkConnectionInterceptor(this)
-        val api = MyApi(networkConnectionInterceptor)
-        val repo = UserRepo(api)
-        val factory = LoginViewModelFactory(repo)
 
         val binding: ActivityLoginBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_login)
